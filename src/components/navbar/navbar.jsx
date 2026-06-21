b75b569cb2d60b5f9fe1cf9c4d58e2bafc20f50a
junaid <jk7948677@gmail.com>
2026-06-21 14:02:18 +0530

    Got a working route & cart swap on click

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,17 +1,52 @@
 // navbar.jsx
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import styles from "./navbar.module.css"
 import cartIcon from "./../../assets/shopping_cart.svg"
+import cartIconGold from "./../../assets/shopping_cart_gold.svg"
 
 export function NavBar () {
     return (
         <div className={styles.navbar}>
-            <h1 className={styles.title}>Placeholder</h1>
-            <div className={styles.navContainer}>
-                <Link to="/" className={styles.navLinks}>Home</Link>
-                <Link to="/shop" className={styles.navLinks}>Shop</Link>
-                <Link to="/cart"><img src={cartIcon} className={styles.navCart} alt="" /></Link>
-            </div>
+            <h1 className={styles.title}>The Shop</h1>
+            <nav>
+                <NavLink 
+                    to="/" 
+                    className={({isActive}) => 
+                        isActive 
+                            ? `${styles.activeLink} ${styles.navLinks}` 
+                            : styles.navLinks
+                    }
+                >
+                    Home
+                </NavLink>
+
+                <NavLink 
+                    to="/shop" 
+                    className={({isActive}) => 
+                        isActive 
+                            ? `${styles.activeLink} ${styles.navLinks}` 
+                            : styles.navLinks
+                    }
+                >
+                    Shop
+                </NavLink>
+
+                <NavLink 
+                    to="/cart"
+                    className={({isActive}) => 
+                        isActive 
+                            ? `${styles.activeLink} ${styles.navCartCont}`
+                            : styles.navCartCont
+                    }
+                >
+                    {({isActive}) => (
+                        isActive 
+                            ? <img src={cartIconGold} className={styles.navCart} alt="" />
+                            : <img src={cartIcon} className={styles.navCart} alt="" />
+                    )}
+                </NavLink>
+
+            </nav>
         </div>
     )
 }
